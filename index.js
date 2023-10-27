2649c8f15f09a2a13e37cc0868df8a6f2e5e13f9
altamisatmaja <altamisfattah16@gmail.com>
2023-10-27 23:50:51 +0700

    update aws

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const app = express();
+const AWS = require("aws-sdk");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+});
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
 
-const PORT = 8000
+app.use(cors());
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+app.use(bodyParser.json());
 
-server.use(cors({
-  origin: 'https://altamisatmaja.biz.id/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  credentials: true,
-}));
+const s3 = new AWS.S3();
+
+app.get('*', async (req, res) => {
+  let filename = req.path.slice(1);
+
+  try {
+    let s3File = await s3.getObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    }).promise();
+
+    res.set('Content-type', s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
+  } catch (error) {
+    if (error.code === 'NoSuchKey') {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
+    } else {
+      console.log(error);
+      res.sendStatus(500).end();
+    }
+  }
+});
+
+app.put('*', async (req, res) => {
+  let filename = req.path.slice(1);
+
+  console.log(typeof req.body);
+
+  await s3.putObject({
+    Body: JSON.stringify(req.body),
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise();
+
+  res.set('Content-type', 'text/plain');
+  res.send('ok').end();
+});
+
+app.delete('*', async (req, res) => {
+  let filename = req.path.slice(1);
+
+  await s3.deleteObject({
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise();
+
+  res.set('Content-type', 'text/plain');
+  res.send('ok').end();
+});
+
+app.use('*', (req, res) => {
+  res.sendStatus(404).end();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
