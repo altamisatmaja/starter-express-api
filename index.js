020d0829ad91bc0b2bb6d402de4f7d90c7efebee
altamisatmaja <altamisfattah16@gmail.com>
2023-10-27 15:45:44 +0700

    add domain

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.listen(PORT, () => {
 })
 
 server.use(cors({
-  origin: 'http://127.0.0.1:5175',
+  origin: 'https://altamisatmaja.biz.id/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
