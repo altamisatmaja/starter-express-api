33fec044ef83aba538b81573db6cefe2f53085c5
altamisatmaja <altamisfattah16@gmail.com>
2023-10-27 15:03:52 +0700

    add server

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
-app.listen(process.env.PORT || 3000)
